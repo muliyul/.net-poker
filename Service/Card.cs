3b31edf1653d0ed1858f2154a2cef0af3742d4e7
Muli Yulzary <muliyul@gmail.com>
2016-02-24 03:19:54 +0200

    Update

---
+++ Service/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Blackjack
+{
+    public enum Suit { Spades, Hearts, Clubs, Diamonds }
+    public enum Face { Two = 2, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack = 10, Queen = 10, King = 10, Ace = 11 }
+
+
+    [DataContract]
+    public class Card
+    {
+        [DataMember]
+        public Suit Suit { get; set; }
+        [DataMember]
+        public Face Face { get; set; }
+        [DataMember]
+        public int Value { get { return (int)Face; } }
+    }
+}
---
+++ Service/Hand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Blackjack
+{
+    [CollectionDataContract]
+    public class Hand : List<Card>
+    {
+        [DataMember]
+        public int Value
+        {
+            get
+            {
+                int sum = this.Sum(card => card.Value);
+                int aces = this.Count(card => card.Face == Face.Ace);
+
+                while (sum > 21 && aces-- > 0) sum -= 10;
+                return sum;
+            }
+        }
+    }
+}
