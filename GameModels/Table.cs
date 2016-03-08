251b84592ec806fcd5fdda440c7ff4392ddd412f
Muli Yulzary <muliyul@gmail.com>
2016-03-08 21:01:25 +0200

    f

---
+++ GameModels/Table.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Web;
+using System.Collections.Specialized;
+
+namespace Shared
+{
+    [DataContract]
+    public class Table
+    {
+        IList<Player> players = new List<Player>();
+
+        [DataMember]
+        public IList<Player> Players { get; set; }
+
+        [DataMember]
+        public string Id { get; set; }
+
+        [DataMember]
+        public int Pot { get; set; }
+    }
+}
